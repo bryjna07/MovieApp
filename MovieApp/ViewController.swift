8be439b9566237e1fb4924a716164ec5fd74af09
Youngjin <bryjna07@nate.com>
2025-07-31 15:24:20 +0900

    Feat: 프로젝트 생성 + Main 스토리보드 삭제

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by YoungJin on 7/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
